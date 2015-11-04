1eff5fb2401c14abef97037e79118c70e1281cb7
mswift42 <martin.haesler@gmail.com>
2015-11-04 20:46:19 +0000

    Add faces-db

--- app/src/cljs/app/db.cljs
+++ app/src/cljs/app/db.cljs
@@ -1,4 +1,18 @@
 (ns app.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:name "atc"})
+
+(def faces-db
+  {:mainbg "#ffffff"
+   :mainfg "#303030"
+   :builtin "#000000"
+   :keyword "#000000"
+   :string  "#000000"
+   :functionname "#000000"
+   :variable "#000000"
+   :type "#000000"
+   :constant "#000000"
+   :comment "#606060"
+   :warning "#ff0000"
+   :warning2 "#ff8800"})
