b9eb9ed19373ffc7c893e13198ae39df09830072
mswift42 <martin.haesler@gmail.com>
2015-11-07 09:55:24 +0000

    add theme-db

--- app/src/cljs/app/db.cljs
+++ app/src/cljs/app/db.cljs
@@ -3,16 +3,16 @@
 (def default-db
   {:name "atc"})
 
-(def faces-db
-  {:mainbg "#ffffff"
-   :mainfg "#303030"
-   :builtin "#000000"
-   :keyword "#000000"
-   :string  "#000000"
-   :functionname "#000000"
-   :variable "#000000"
-   :type "#000000"
-   :constant "#000000"
-   :comment "#606060"
-   :warning "#ff0000"
-   :warning2 "#ff8800"})
+(def theme-db
+  {:theme {:mainbg "#ffffff"
+           :mainfg "#303030"
+           :builtin "#000000"
+           :keyword "#000000"
+           :string  "#000000"
+           :functionname "#000000"
+           :variable "#000000"
+           :type "#000000"
+           :constant "#000000"
+           :comment "#606060"
+           :warning "#ff0000"
+           :warning2 "#ff8800"}})
